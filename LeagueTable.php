5baaa8dd9d84625749c9f156939482d211afb311
Mengyi <39472093+mengyi1028@users.noreply.github.com>
2020-10-12 22:07:42 -0400

    Create LeagueTable.php

---
+++ LeagueTable.php
@@ -0,0 +1,49 @@
+<?php
+
+class LeagueTable
+{
+    public function __construct(array $players)
+    {
+        $this->standings = [];
+        foreach($players as $index => $p) {
+            $this->standings[$p] = [
+                'index'        => $index,
+                'games_played' => 0,
+                'score'        => 0
+            ];
+        }
+    }
+
+    public function recordResult(string $player, int $score) : void
+    {
+        $this->standings[$player]['games_played']++;
+        $this->standings[$player]['score'] += $score;
+    }
+    
+    public function playerRank(int $rank)
+    { 
+        function sortByScore($a, $b){
+        if($a["score"]==$b["score"])
+           return sortByGame($a, $b);
+       return ($a["score"]<$b["score"])?-1:1;
+    }
+     function sortByGame($a, $b){
+        if($a["games_played"]==$b["games_played"])
+           return ($a["index"]<$b["index"])?-1:1;;
+       return ($a["games_played"]<$b["games_played"])?-1:1;
+    }
+       $newArr=$this->standings;
+        uasort($newArr, "sortByScore");
+        $allPlayers=array_keys($newArr);
+        return  $allPlayers[$rank-1];
+    }
+    
+    
+}
+
+$table = new LeagueTable(array('Mike', 'Chris', 'Arnold'));
+$table->recordResult('Mike', 2);
+$table->recordResult('Mike', 3);
+$table->recordResult('Arnold', 5);
+$table->recordResult('Chris', 5);
+var_dump( $table->playerRank(1));
